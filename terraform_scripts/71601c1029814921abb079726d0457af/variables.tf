18e3e9a81e8573bc0025f68ce5d2217159d836a6
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-20 11:46:39 +0530

    arch-gen

---
+++ terraform_scripts/71601c1029814921abb079726d0457af/variables.tf
@@ -0,0 +1,14 @@
+variable "sql_admin_username" {
+  description = "The SQL admin username"
+  type        = string
+}
+
+variable "sql_admin_password" {
+  description = "The SQL admin password"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant id"
+  type        = string
+}
