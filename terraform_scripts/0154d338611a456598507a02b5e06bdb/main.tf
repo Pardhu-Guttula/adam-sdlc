25bc2b3c907201492b61ccde39e392fa61a16d95
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-26 17:42:15 +0530

    arch-gen

---
+++ terraform_scripts/0154d338611a456598507a02b5e06bdb/main.tf
@@ -0,0 +1,114 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+    app_settings = {
+      "WEBSITE_NODE_DEFAULT_VERSION" = "10.14"
+    }
+  }
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_administrator_login
+  administrator_login_password = var.sql_administrator_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-sql-database"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  requested_service_objective_name = "S0"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks"
+  linux_profile {
+    admin_username = "aksuser"
+    ssh_key {
+      key_data = file(var.ssh_public_key_path)
+    }
+  }
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_DS2_v2"
+    os_disk_size_gb     = 30
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "example-ai"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example-diagnostic"
+  target_resource_id         = azurerm_app_service.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+    enabled  = true
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-law"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "Your Company"
+  publisher_email     = "company@example.com"
+  sku_name            = "Developer_1"
+}
