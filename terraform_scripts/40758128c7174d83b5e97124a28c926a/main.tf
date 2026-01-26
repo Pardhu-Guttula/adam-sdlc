6546c8aa9a75c8d49762be418941bc2bbdba2e49
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-26 15:46:13 +0530

    arch-gen

---
+++ terraform_scripts/40758128c7174d83b5e97124a28c926a/main.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "mysqlserver2022"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "H@Sh1CoR3!"
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-db"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+
+  requested_service_objective_name = "S0"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+}
