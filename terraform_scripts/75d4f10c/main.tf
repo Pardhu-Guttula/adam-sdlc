d474cd5f7083d8e79e42801a9e7bcd33b4cb8fa3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-27 15:46:01 +0530

    arch-gen

---
+++ terraform_scripts/75d4f10c/main.tf
@@ -0,0 +1,89 @@
+provider "azurerm" {
+  features {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_frontdoor" "example" {
+  name                                         = "example-frontdoor"
+  resource_group_name                          = azurerm_resource_group.example.name
+  backend_pool_health_probe_setting {
+    name     = "healthprobe1"
+    resource {
+      type = "FrontDoor/healthier"
+    }
+  }
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "example-keyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id           = var.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = var.sql_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  sku_name            = "S0"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdbaccount"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_monitor" "example" {
+}
