a680ca42f42354988b31f1e7aea45bbdf4d58fa6
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-31 15:35:13 +0530

    arch-gen

---
+++ terraform_scripts/186920eb051347c78b27830645346a6e/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "example-resources"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
