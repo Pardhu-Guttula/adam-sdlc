ca6d9052c2f78fb66332d4df6e3e6dd1d758cb06
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-31 15:04:32 +0530

    arch-gen

---
+++ terraform_scripts/3abcd066f74f4a42a693de10426a55dd/variables.tf
@@ -0,0 +1,14 @@
+variable "sql_admin_username" {
+  description = "The SQL server admin username."
+  type        = string
+}
+
+variable "sql_admin_password" {
+  description = "The SQL server admin password."
+  type        = string
+}
+
+variable "principal_id" {
+  description = "The Principal ID for Role Assignment."
+  type        = string
+}
