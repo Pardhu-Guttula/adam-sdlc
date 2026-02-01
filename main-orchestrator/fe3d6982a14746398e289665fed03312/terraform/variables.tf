a8867b1cfc4f79f023fb2b53f19f4f011c3902fe
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-01 13:20:04 +0530

    arch-gen

---
+++ main-orchestrator/fe3d6982a14746398e289665fed03312/terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be created"
+  type        = string
+}
+
+variable "sql_admin_username" {
+  description = "The administrator username for the SQL server"
+  type        = string
+}
+
+variable "sql_admin_password" {
+  description = "The administrator password for the SQL server"
+  type        = string
+  sensitive   = true
+}
