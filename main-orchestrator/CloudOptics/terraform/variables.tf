fdc690dcf81ae828346cd28a0a8b1b7c0897819c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-07 19:10:19 +0530

    arch-gen

--- main-orchestrator/CloudOptics/terraform/variables.tf
+++ main-orchestrator/CloudOptics/terraform/variables.tf
@@ -1,19 +1,38 @@
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources"
+  description = "The name of the resource group"
   type        = string
+  default     = "example-resources"
 }
 
 variable "location" {
-  description = "The location/region where the resources will be created"
+  description = "The Azure Region"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "admin_username" {
+  description = "The admin username for the VM"
   type        = string
 }
 
+variable "admin_password" {
+  description = "The admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
 variable "sql_admin_username" {
-  description = "The admin username for the SQL Server"
+  description = "The admin username for SQL server"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The admin password for the SQL Server"
+  description = "The admin password for SQL server"
+  type        = string
+  sensitive   = true
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics Workspace"
   type        = string
 }
