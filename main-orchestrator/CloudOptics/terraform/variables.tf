13e953a0c7408bc6fcdbc395f1e409712af06d9d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-07 15:53:50 +0530

    arch-gen

--- main-orchestrator/CloudOptics/terraform/variables.tf
+++ main-orchestrator/CloudOptics/terraform/variables.tf
@@ -1,10 +1,23 @@
 variable "resource_group_name" {
-  description = "The name of the resource group in which resources are created."
+  description = "The name of the resource group in which to create the resources."
   type        = string
+  default     = "example-resources"
 }
 
 variable "location" {
-  description = "The Azure region where resources will be created."
+  description = "The Azure region where the resources will be created."
   type        = string
   default     = "East US"
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine."
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "The admin password for the virtual machine."
+  type        = string
+  default     = "P@ssw0rd1234"
 }
