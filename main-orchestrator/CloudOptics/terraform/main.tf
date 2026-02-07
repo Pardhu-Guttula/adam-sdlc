0b6e4cdbe66407ba6ebc877fd839944ea5474adf
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-07 19:10:18 +0530

    arch-gen

--- main-orchestrator/CloudOptics/terraform/main.tf
+++ main-orchestrator/CloudOptics/terraform/main.tf
@@ -1,5 +1,15 @@
 provider "azurerm" {
   features {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+}
+
+provider "azuread" {
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
@@ -7,39 +17,115 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                        = "example-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_DS1_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
 resource "azurerm_app_service_plan" "example" {
   name                = "example-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-
   sku {
     tier = "Basic"
     size = "B1"
   }
 }
 
-resource "azurerm_app_service" "frontend" {
-  name                = "example-frontend-app"
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+    scm_type = "LocalGit"
+  }
 }
 
-resource "azurerm_app_service" "backend" {
-  name                = "example-backend-app"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_sql_server" "example" {
+  name                         = "example_sql_server"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_function_app" "backend_functions" {
-  name                = "example-backend-functions"
-  location            = azurerm_resource_group.example.location
+resource "azurerm_sql_database" "example" {
+  name                = "example_database"
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-  storage_account_name = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  functions_version   = "3"
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  requested_service_objective_name = "S0"
 }
 
 resource "azurerm_storage_account" "example" {
@@ -50,34 +136,16 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "mysqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
-}
-
-resource "azurerm_sql_database" "example" {
-  name                = "example-sqldb"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  requested_service_objective_name = "S1"
-}
-
-resource "azurerm_cosmosdb_account" "example" {
-  name                = "examplecosmosdb"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-}
-
-resource "azurerm_cosmosdb_sql_container" "example" {
-  name                = "example-container"
-  cosmosdb_account_id = azurerm_cosmosdb_account.example.id
-  database_name       = "exampledb"
-  partition_key_path  = "/partitionKey"
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example-diagnostic-setting"
+  target_resource_id         = azurerm_storage_account.example.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+  enabled_log {
+    category = "StorageRead"
+    enabled  = true
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
 }
