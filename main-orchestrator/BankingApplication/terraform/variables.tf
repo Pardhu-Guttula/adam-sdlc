d5115520d4193158bc4a83365ecaeab4e21286ca
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-08 20:56:43 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,39 +1,21 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
 variable "location" {
-  description = "The Azure location where resources will be deployed."
+  description = "The Azure region to deploy to."
+  type        = string
+  default     = "East US"
+}
+
+variable "sql_admin_username" {
+  description = "The username of the SQL administrator user."
   type        = string
 }
 
-variable "asp_name" {
-  description = "The name of the App Service Plan."
+variable "sql_admin_password" {
+  description = "The password of the SQL administrator user."
   type        = string
+  sensitive   = true
 }
 
-variable "app_service_name" {
-  description = "The name of the App Service."
-  type        = string
-}
-
-variable "logic_app_name" {
-  description = "The name of the Logic App."
-  type        = string
-}
-
-variable "diag_setting_name" {
-  description = "The name of the Diagnostic Setting."
-  type        = string
-}
-
-variable "law_name" {
-  description = "The name of the Log Analytics Workspace."
-  type        = string
-}
-
-variable "app_insights_name" {
-  description = "The name of the Application Insights."
+variable "principal_id" {
+  description = "The principal ID for RBAC assignment."
   type        = string
 }
