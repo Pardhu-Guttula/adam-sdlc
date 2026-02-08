04659399c62b353b18b2f0aecc51601c9aa51e86
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-08 22:17:51 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -3,58 +3,7 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "resource_group_location" {
+variable "location" {
   description = "Location of the resource group"
-  type        = string
-}
-
-variable "app_service_plan_name" {
-  description = "Name of the App Service plan"
-  type        = string
-}
-
-variable "web_app_name" {
-  description = "Name of the Web App"
-  type        = string
-}
-
-variable "app_insights_name" {
-  description = "Name of the Application Insights instance"
-  type        = string
-}
-
-variable "diagnostic_setting_name" {
-  description = "Name of the Diagnostic Setting"
-  type        = string
-}
-
-variable "log_analytics_workspace_id" {
-  description = "ID of the Log Analytics workspace"
-  type        = string
-}
-
-variable "sql_server_name" {
-  description = "Name of the SQL server"
-  type        = string
-}
-
-variable "sql_admin_username" {
-  description = "Administrator username for the SQL server"
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "Administrator password for the SQL server"
-  type        = string
-  sensitive   = true
-}
-
-variable "sql_database_name" {
-  description = "Name of the SQL database"
-  type        = string
-}
-
-variable "redis_cache_name" {
-  description = "Name of the Redis cache instance"
   type        = string
 }
