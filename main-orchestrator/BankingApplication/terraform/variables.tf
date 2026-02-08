801fd1c5d63d97ec2526e71c5f45237d3cc41c62
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-08 21:57:50 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,21 +1,60 @@
-variable "location" {
-  description = "The Azure region to deploy to."
+variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
-  default     = "East US"
+}
+
+variable "resource_group_location" {
+  description = "Location of the resource group"
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service plan"
+  type        = string
+}
+
+variable "web_app_name" {
+  description = "Name of the Web App"
+  type        = string
+}
+
+variable "app_insights_name" {
+  description = "Name of the Application Insights instance"
+  type        = string
+}
+
+variable "diagnostic_setting_name" {
+  description = "Name of the Diagnostic Setting"
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "ID of the Log Analytics workspace"
+  type        = string
+}
+
+variable "sql_server_name" {
+  description = "Name of the SQL server"
+  type        = string
 }
 
 variable "sql_admin_username" {
-  description = "The username of the SQL administrator user."
+  description = "Administrator username for the SQL server"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The password of the SQL administrator user."
+  description = "Administrator password for the SQL server"
   type        = string
   sensitive   = true
 }
 
-variable "principal_id" {
-  description = "The principal ID for RBAC assignment."
+variable "sql_database_name" {
+  description = "Name of the SQL database"
+  type        = string
+}
+
+variable "redis_cache_name" {
+  description = "Name of the Redis cache instance"
   type        = string
 }
