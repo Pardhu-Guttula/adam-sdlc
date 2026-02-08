0c981b2950d7600ca2718f00e981d3a7663a6b30
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-08 19:53:22 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/main.tf
+++ main-orchestrator/BankingApplication/terraform/main.tf
@@ -1,94 +1,68 @@
-provider 'azurerm' {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+}
+
+provider "azurerm" {
   features {}
 }
 
-resource 'azurerm_resource_group' 'rg' {
-  name     = 'example-resources'
-  location = 'East US'
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource 'azurerm_storage_account' 'sa' {
-  name                     = 'examplestoracc'
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = 'Standard'
-  account_replication_type = 'LRS'
-}
-
-resource 'azurerm_sql_server' 'sql' {
-  name                         = 'sqlserverexample'
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = '12.0'
-  administrator_login          = 'sqladmin'
-  administrator_login_password = var.sql_password
-}
-
-resource 'azurerm_sql_database' 'sqldb' {
-  name                = 'mysqldb'
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql.name
-  requested_service_objective_name = 'S0'
-}
-
-resource 'azurerm_api_management' 'apim' {
-  name                = 'example-apim'
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.asp_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = 'My Company'
-  publisher_email     = 'company@example.com'
-  sku_name            = 'Developer_1'
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
-resource 'azurerm_cognitive_account' 'cognitive' {
-  name                = 'examplecognitive'
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_app_service" "app" {
+  name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
-  kind                = 'CognitiveServices'
-  sku_name            = 'S0'
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 }
 
-resource 'azurerm_monitor_diagnostic_setting' 'diagnostic' {
-  name                       = 'example-diagnostics'
-  target_resource_id         = azurerm_sql_server.sql.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
+resource "azurerm_logic_app_workflow" "logic_app" {
+  name                = var.logic_app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diag_setting" {
+  name               = var.diag_setting_name
+  target_resource_id = azurerm_app_service.app.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
   enabled_log {
-    category = 'SQLInsights'
+    category = "AppServiceHTTPLogs"
   }
 
   metric {
-    category = 'AllMetrics'
-  }
-
-}
-
-resource 'azurerm_log_analytics_workspace' 'log' {
-  name                = 'example-logs'
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = 'PerGB2018'
-  retention_in_days   = 30
-}
-
-resource 'azurerm_monitor_metric_alert' 'alert' {
-  name                = 'example-alert'
-  resource_group_name = azurerm_resource_group.rg.name
-  scopes              = [azurerm_sql_server.sql.id]
-  description         = 'Action will be triggered on high CPU usage'
-  criteria {
-    metric_name = 'cpu_percentage'
-    aggregation = 'Average'
-    operator    = 'GreaterThan'
-    threshold   = 90
+    category = "AllMetrics"
   }
 }
 
-resource 'azurerm_active_directory_domain_service' 'adds' {
-  name                = 'example-adds'
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.law_name
   location            = azurerm_resource_group.rg.location
-  domain_name         = 'example.com'
-  sku                 = 'Standard'
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = var.app_insights_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
 }
