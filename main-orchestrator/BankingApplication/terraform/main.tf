d463159c3404ab2ffb8d21fa64f403f858bdb71c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-08 20:56:42 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/main.tf
+++ main-orchestrator/BankingApplication/terraform/main.tf
@@ -5,64 +5,154 @@ terraform {
       version = "~> 4.56.0"
     }
   }
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_static_site" "example" {
+  name                = "exampleStaticSite"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Free"
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = var.asp_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_cdn_endpoint" "example" {
+  name                = "cdn-endpoint"
+  profile_name        = azurerm_cdn_profile.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  origins {
+    name      = "example-origin"
+    host_name = azurerm_static_site.example.default_domain
+  }
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "Example Publisher"
+  publisher_email     = "example@publisher.com"
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+    app_settings = {
+      "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    }
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-resource "azurerm_app_service" "app" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_logic_app_workflow" "logic_app" {
-  name                = var.logic_app_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  requested_service_objective_name = "S0"
 }
 
-resource "azurerm_monitor_diagnostic_setting" "diag_setting" {
-  name               = var.diag_setting_name
-  target_resource_id = azurerm_app_service.app.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-  enabled_log {
-    category = "AppServiceHTTPLogs"
+resource "azurerm_event_grid_topic" "example" {
+  name                = "example-eventgrid"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "exampleservicebus"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_notification_hub_namespace" "example" {
+  name                = "example-namespace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Free"
+}
+
+resource "azurerm_signalr_service" "example" {
+  name                = "example-signalr"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    name     = "Free_F1"
   }
-
-  metric {
-    category = "AllMetrics"
-  }
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = var.law_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
+resource "azurerm_monitor" "example" {
+  name                = "example-monitor"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_application_insights" "app_insights" {
-  name                = var.app_insights_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_application_insights" "example" {
+  name                = "example-ai"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   application_type    = "web"
+}
+
+resource "azurerm_security_center_contact" "example" {
+  email              = "example@domain.com"
+  phone              = "123-456-7890"
+  alert_notifications = "On"
+}
+
+resource "azurerm_policy_assignment" "example" {
+  name                 = "example-policy"
+  scope                = azurerm_resource_group.example.id
+  policy_definition_id = azurerm_policy_definition.example.id
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Contributor"
+  principal_id         = var.principal_id
+}
+
+resource "azurerm_devops_organization" "example" {
+  name     = "example-org"
+  location = azurerm_resource_group.example.location
+}
+
+resource "azurerm_devops_project" "example" {
+  name                = "example-project"
+  project_name        = "Example Project"
+  organization_name   = azurerm_devops_organization.example.name
 }
