7e9e1c0269c6d692536c377145e8426996c59d78
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 13:18:40 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/main.tf
+++ main-orchestrator/BankingApplication/terraform/main.tf
@@ -1,58 +1,109 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "appserviceplan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_app_service" "app_service" {
+  name                = "webapp"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+  site_config {
+    always_on                = true
+    dotnet_framework_version = "v4.0"
+  }
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "examplesqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "akscluster"
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_sql_server" "sql_server" {
+  name                         = "sqlserver"
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_sql_database" "example" {
-  name                = "exampledatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  sku_name            = "S0"
+resource "azurerm_sql_database" "sql_database" {
+  name                = "sqldatabase"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  server_name         = azurerm_sql_server.sql_server.name
+  requested_service_objective_name = "S1"
 }
 
-resource "azurerm_b2c_directory" "example" {
-  name                = var.b2c_directory_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_cosmosdb_account" "cosmosdb" {
+  name                = "cosmosdb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_api_management" "api_gateway" {
+  name                = "apimanagement"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@example.com"
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                       = "keyvault"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  tenant_id                  = var.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = "appi"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  application_type    = "web"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "storageaccount"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
