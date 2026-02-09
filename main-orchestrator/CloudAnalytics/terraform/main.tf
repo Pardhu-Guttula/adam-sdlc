b4de99ca36bdf83f1b6a06850d0d85a2fd221b9e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 12:42:32 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -1,35 +1,44 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+  acl    = "private"
+}
+
+resource "google_storage_bucket" "example" {
+  name                        = "example-bucket"
+  location                    = var.gcp_region
+  force_destroy               = true
+  storage_class               = "STANDARD"
+}
+
+output "azurerm_storage_account_primary_endpoint" {
+  value = azurerm_storage_account.example.primary_endpoint
+}
+
+output "aws_s3_bucket_arn" {
+  value = aws_s3_bucket.example.arn
+}
+
+output "google_storage_bucket_url" {
+  value = google_storage_bucket.example.url
 }
