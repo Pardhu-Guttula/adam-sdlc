96585b4980711e619fc6fdfc96cfacbdae250454
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 14:43:56 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/variables.tf
+++ main-orchestrator/CloudAnalytics/terraform/variables.tf
@@ -1,24 +1,34 @@
+variable "azure_location" {
+  description = "The location of the azure resources."
+  type        = string
+}
+
+variable "azure_rg" {
+  description = "The name of the resource group in Azure."
+  type        = string
+}
+
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "The AWS region to create resources in."
   type        = string
 }
 
-variable "gcp_project" {
-  description = "The GCP project ID"
+variable "google_project" {
+  description = "The Google project ID."
   type        = string
 }
 
-variable "gcp_region" {
-  description = "The GCP region to deploy resources"
+variable "google_region" {
+  description = "The Google cloud region to create resources in."
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The Azure Resource Group name"
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace."
   type        = string
 }
 
-variable "location" {
-  description = "The Azure region to deploy resources"
+variable "principal_id" {
+  description = "The principal ID for the role assignment."
   type        = string
 }
