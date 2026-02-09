fbefba6c29c5a96986dd887b19697e3fb167bc7f
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 12:26:44 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -4,77 +4,32 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.56.0"
     }
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.0"
-    }
   }
+  required_version = ">= 0.13"
 }
 
 provider "azurerm" {
   features {}
 }
 
-provider "aws" {
-  region = var.aws_region
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
-resource "azurerm_role_definition" "example" {
-  name                            = "ExampleRole"
-  role_definition_id              = uuid()
-  scope                           = "/subscriptions/${data.azurerm_subscription.primary.subscription_id}"
-  permissions {
-    actions   = ["*"]
-    not_actions = []
-  }
-  assignable_scopes              = ["/subscriptions/${data.azurerm_subscription.primary.subscription_id}"]
-}
-
-resource "aws_iam_role" "example" {
-  name = "example_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "azurerm_cost_management_export" "example" {
-  name                = "example-export"
-  resource_group_name = var.resource_group_name
-  storage_account_id  = var.storage_account_id
-  recurrence_period {
-    from_date = "YYYY-MM-DD"
-    to_date   = "YYYY-MM-DD"
-    type      = "Monthly"
-  }
-  schedule {
-    recurrence   = "Annually"
-    recurrence_years = [var.recurrence_year]
-  }
-  format = "Csv"
-}
-
-resource "aws_ce_cost_category" "example" {
-  name = "example"
-  rules {
-    type = "REGULAR"
-    value = "Testing"
-    rule_version = "Cost Category Expression V1"
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
   }
 }
 
-resource "azurerm_security_center" "example" {
-  pricing {
-    tier = "Free"
-  }
-}
-
-resource "aws_securityhub_account" "example" {
-  depends_on = [aws_iam_role.example]
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
