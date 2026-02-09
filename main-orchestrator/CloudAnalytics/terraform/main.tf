fe7a9f8211d8dd5463e3b7ea85e3c54d9a7035dd
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 15:05:15 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -1,25 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 provider "azurerm" {
   features {}
 }
 
-provider "aws" {
-  region = "us-east-1"
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+  }
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "East US"
+  location = var.azure_location
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "f1-micro"
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
-  acl    = "private"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
