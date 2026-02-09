94ae647469f5b205c93109dd4ec3f4ad45311a82
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 15:05:16 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/variables.tf
+++ main-orchestrator/CloudAnalytics/terraform/variables.tf
@@ -1,11 +1,22 @@
 variable "aws_region" {
-  description = "The AWS region."
+  description = "The AWS region to deploy resources in."
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "azure_location" {
-  description = "The Azure location."
+  description = "The Azure region to deploy resources in."
   type        = string
-  default     = "East US"
+  default     = "West US"
+}
+
+variable "gcp_project" {
+  description = "The GCP project to deploy resources in."
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "The GCP region to deploy resources in."
+  type        = string
+  default     = "us-central1"
 }
