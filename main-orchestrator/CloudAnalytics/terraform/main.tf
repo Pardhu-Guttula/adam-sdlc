963f047d850e6d854531586869273c90de6d0943
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 15:34:25 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -8,6 +8,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "~> 1.0"
+    }
   }
 }
 
@@ -19,23 +27,41 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "azurerm_user_assigned_identity" "example" {
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  name                = "exampleIdentity"
+provider "google" {
+  project = var.google_project
+  region  = var.google_region
 }
 
-resource "aws_iam_role" "example" {
-  name               = "exampleRole"
-  assume_role_policy = "${data.aws_iam_policy_document.example.json}"
+provider "ibm" {
 }
 
-resource "aws_iam_policy_document" "example" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.azure_location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+  acl    = "private"
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "example-bucket"
+  location = var.google_region
+}
+
+resource "ibm_resource_instance" "example" {
+  name          = "example-instance"
+  service       = "cloud-object-storage"
+  plan          = "lite"
+  location      = var.ibm_location
+  resource_group = "Default"
 }
