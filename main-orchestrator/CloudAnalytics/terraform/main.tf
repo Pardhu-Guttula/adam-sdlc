5ec0c5983417cb3d8e52c39b486fbdd04b66f2de
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 12:03:13 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -1,71 +1,80 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_active_directory_domain_service" "example" {
-  name                = "example"
-  resource_group_name = "example-resources"
-  location            = "West US"
-  domain_name         = "example.com"
+provider "aws" {
+  region = var.aws_region
+}
 
-  sku {
-    name = "Standard"
+resource "azurerm_role_definition" "example" {
+  name                            = "ExampleRole"
+  role_definition_id              = uuid()
+  scope                           = "/subscriptions/${data.azurerm_subscription.primary.subscription_id}"
+  permissions {
+    actions   = ["*"]
+    not_actions = []
   }
+  assignable_scopes              = ["/subscriptions/${data.azurerm_subscription.primary.subscription_id}"]
+}
 
-  ldaps_settings {
-    external_access_enabled = true
-    ldaps {
-      enabled = true
-      protocol = "Tcp"
-      certificate = "---CERTIFICATE---"
-    }
+resource "aws_iam_role" "example" {
+  name = "example_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "azurerm_cost_management_export" "example" {
+  name                = "example-export"
+  resource_group_name = var.resource_group_name
+  storage_account_id  = var.storage_account_id
+  recurrence_period {
+    from_date = "YYYY-MM-DD"
+    to_date   = "YYYY-MM-DD"
+    type      = "Monthly"
   }
+  schedule {
+    recurrence   = "Annually"
+    recurrence_years = [var.recurrence_year]
+  }
+  format = "Csv"
+}
 
-  notification_settings {
-    notify_global_admins = true
-    notify_user_roles    = ["User Administrator"]
+resource "aws_ce_cost_category" "example" {
+  name = "example"
+  rules {
+    type = "REGULAR"
+    value = "Testing"
+    rule_version = "Cost Category Expression V1"
   }
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks"
-  location            = azurerm_active_directory_domain_service.example.location
-  resource_group_name = azurerm_active_directory_domain_service.example.resource_group_name
-  dns_prefix          = "exampleaks"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-  }
-
-  identity {
-    type = "SystemAssigned"
+resource "azurerm_security_center" "example" {
+  pricing {
+    tier = "Free"
   }
 }
 
-resource "azurerm_policy_assignment" "example" {
-  name                 = "example-policy-assignment"
-  scope                = azurerm_active_directory_domain_service.example.id
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/ExamplePolicy"
-}
-
-resource "azurerm_monitor_diagnostic_setting" "example" {
-  name                       = "example-diagnostic-setting"
-  target_resource_id         = azurerm_active_directory_domain_service.example.id
-  log_analytics_workspace_id = "/subscriptions/XXX/resourceGroups/YYY/providers/Microsoft.OperationalInsights/workspaces/ZZZ"
-  enabled_log {
-    category = "Administrative"
-    enabled  = true
-    retention_policy_days = 0
-  }
-}
-
-resource "azurerm_key_vault" "example" {
-  name                = "examplekv"
-  location            = azurerm_active_directory_domain_service.example.location
-  resource_group_name = azurerm_active_directory_domain_service.example.resource_group_name
-  tenant_id           = "${var.tenant_id}"
-  sku {
-    name = "standard"
-  }
+resource "aws_securityhub_account" "example" {
+  depends_on = [aws_iam_role.example]
 }
