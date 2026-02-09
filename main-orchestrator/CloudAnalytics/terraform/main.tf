8dadb7507c8ebd2459dc0c2f40aaa965f4426bf3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 15:17:20 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/main.tf
+++ main-orchestrator/CloudAnalytics/terraform/main.tf
@@ -1,64 +1,41 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.56.0"
     }
-    google = {
-      source = "hashicorp/google"
+    aws = {
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
 
 provider "azurerm" {
   features {}
 }
 
-provider "google" {
-  project = var.gcp_project
-  region  = var.gcp_region
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+resource "azurerm_user_assigned_identity" "example" {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  name                = "exampleIdentity"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
-  }
+resource "aws_iam_role" "example" {
+  name               = "exampleRole"
+  assume_role_policy = "${data.aws_iam_policy_document.example.json}"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.azure_location
-}
-
-resource "google_compute_instance" "example" {
-  name         = "example-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+resource "aws_iam_policy_document" "example" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
     }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
   }
 }
