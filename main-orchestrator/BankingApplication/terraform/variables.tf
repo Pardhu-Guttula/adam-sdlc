e25ef944a64ec56eb7462ff1c679bace27ac2133
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 13:18:41 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,21 +1,27 @@
-variable "location" {
-  description = "The Azure region where resources will be created."
+variable "resource_group_name" {
+  description = "The name of the resource group"
   type        = string
-  default     = "East US"
+  default     = "main-resource-group"
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  default     = "West Europe"
 }
 
 variable "sql_admin_username" {
-  description = "The username for the SQL Server administrator."
+  description = "The administrator username for SQL Server"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The password for the SQL Server administrator."
+  description = "The administrator password for SQL Server"
   type        = string
   sensitive   = true
 }
 
-variable "b2c_directory_name" {
-  description = "The name of the Azure B2C Directory."
+variable "tenant_id" {
+  description = "The tenant ID for the subscription"
   type        = string
 }
