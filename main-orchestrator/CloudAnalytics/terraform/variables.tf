13b19fe53a0dfe6fe2fbdec8176b9595c547992f
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-09 14:59:02 +0530

    arch-gen

--- main-orchestrator/CloudAnalytics/terraform/variables.tf
+++ main-orchestrator/CloudAnalytics/terraform/variables.tf
@@ -1,34 +1,30 @@
-variable "azure_location" {
-  description = "The location of the azure resources."
-  type        = string
-}
-
-variable "azure_rg" {
-  description = "The name of the resource group in Azure."
-  type        = string
-}
-
 variable "aws_region" {
-  description = "The AWS region to create resources in."
+  description = "AWS region"
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "gcp_project" {
+  description = "GCP project ID"
   type        = string
 }
 
-variable "google_project" {
-  description = "The Google project ID."
+variable "assume_role_policy" {
+  description = "IAM assume role policy"
   type        = string
 }
 
-variable "google_region" {
-  description = "The Google cloud region to create resources in."
-  type        = string
-}
-
-variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics workspace."
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
 }
 
 variable "principal_id" {
-  description = "The principal ID for the role assignment."
+  description = "Azure Principal ID"
+  type        = string
+}
+
+variable "ibm_iam_id" {
+  description = "IBM IAM ID"
   type        = string
 }
