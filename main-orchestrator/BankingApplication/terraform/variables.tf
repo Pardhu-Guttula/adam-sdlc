7c216261d92f293757a1a9c596bd197049cae30d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 15:34:26 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,11 +1,9 @@
 variable "resource_group_name" {
-  description = "The name of the resource group to create or use"
+  description = "The name of the resource group in which to create resources."
   type        = string
-  default     = "example-resources"
 }
 
 variable "location" {
-  description = "The Azure region where resources will be created"
+  description = "The Azure location where resources should be created."
   type        = string
-  default     = "East US"
 }
