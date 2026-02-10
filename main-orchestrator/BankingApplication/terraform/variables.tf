6af5b94a7fc875072acadd5733465af51b2ecd83
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 23:23:46 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,22 +1,64 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
+variable "prefix" {
+  description = "Prefix for resources"
   type        = string
+  default     = "example"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "example-resources"
 }
 
 variable "location" {
-  description = "The location/region where the resources will be created."
+  description = "Azure region"
   type        = string
   default     = "East US"
 }
 
-variable "sql_admin_password" {
-  description = "The password of the SQL server administrator."
+variable "sql_admin_username" {
+  description = "SQL Administrator Username"
   type        = string
-  sensitive   = true
+  default     = "sqladmin"
 }
 
-variable "key_vault_secret_value" {
-  description = "The value to be stored in the Key Vault as a secret."
+variable "sql_admin_password" {
+  description = "SQL Administrator Password"
   type        = string
-  sensitive   = true
+  default     = "P@ssword1234"
+}
+
+variable "publisher_name" {
+  description = "API Management Publisher Name"
+  type        = string
+  default     = "publisher"
+}
+
+variable "publisher_email" {
+  description = "API Management Publisher Email"
+  type        = string
+  default     = "publisher@example.com"
+}
+
+variable "sendgrid_username" {
+  description = "SendGrid Username"
+  type        = string
+  default     = "sendgriduser"
+}
+
+variable "sendgrid_password" {
+  description = "SendGrid Password"
+  type        = string
+  default     = "SendGridP@ssword123"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "diagnostic_setting_name" {
+  description = "Name for the diagnostic setting"
+  type        = string
+  default     = "diagnostic-setting"
 }
