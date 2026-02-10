d02457bf30d71e05b7390c18e2831b4237fa32be
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 23:18:14 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,11 +1,22 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the resources."
+  type        = string
+}
+
 variable "location" {
-  description = "The Azure region where resources will be created."
+  description = "The location/region where the resources will be created."
   type        = string
   default     = "East US"
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group."
+variable "sql_admin_password" {
+  description = "The password of the SQL server administrator."
   type        = string
-  default     = "example-resources"
+  sensitive   = true
+}
+
+variable "key_vault_secret_value" {
+  description = "The value to be stored in the Key Vault as a secret."
+  type        = string
+  sensitive   = true
 }
