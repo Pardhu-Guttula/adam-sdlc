1061d213c7b19842dbe584361fa1170159915abd
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 23:36:06 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,4 +1,17 @@
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+  default     = "main-resource-group"
+}
+
 variable "location" {
-  description = "The location where resources will be created."
+  description = "Location where resources will be deployed."
+  type        = string
   default     = "East US"
+}
+
+variable "ad_b2c_directory_name" {
+  description = "Azure Active Directory B2C Directory Name."
+  type        = string
+  default     = "myb2cdir"
 }
