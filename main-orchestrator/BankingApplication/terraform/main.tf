97f426ad6f6f4f78365dbb76cdbde402a228e9d7
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 23:36:05 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/main.tf
+++ main-orchestrator/BankingApplication/terraform/main.tf
@@ -1,51 +1,94 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "main_rg" {
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "frontend" {
-  name                = "frontend-appservice-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "App"
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "app-service-plan"
+  resource_group_name = azurerm_resource_group.main_rg.name
+  location            = azurerm_resource_group.main_rg.location
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_app_service" "frontend" {
-  name                = "frontend-app"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.frontend.id
+resource "azurerm_app_service" "app_service" {
+  name                = "frontend-web-app"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
 }
 
-resource "azurerm_traffic_manager_profile" "frontend" {
-  name                = "frontend-traffic-manager"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  profile_status      = "Enabled"
-  traffic_routing_method = "Performance"
-  dns_config {
-    relative_name = "frontend-traffic-manager"
-    ttl           = 60
-  }
-  monitor_config {
-    protocol = "HTTP"
-    port     = 80
-    path     = "/"
-  }
+resource "azurerm_app_service" "backend_service" {
+  name                = "backend-service"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
 }
 
-resource "azurerm_traffic_manager_endpoint" "frontend" {
-  name                = "frontend-app-endpoint"
-  resource_group_name = azurerm_resource_group.example.name
-  traffic_manager_profile_name = azurerm_traffic_manager_profile.frontend.name
-  type                = "azureEndpoints"
-  target_resource_id  = azurerm_app_service.frontend.id
+resource "azurerm_function_app" "function_app" {
+  name                = "azure-functions"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+  storage_account_name = azurerm_storage_account.functions_storage.name
+  storage_account_access_key = azurerm_storage_account.functions_storage.primary_access_key
+  os_type = "linux"
+  runtime_stack = "node"
+}
+
+resource "azurerm_storage_account" "functions_storage" {
+  name                     = "functionsstorage${random_string.random.result}"
+  resource_group_name      = azurerm_resource_group.main_rg.name
+  location                 = azurerm_resource_group.main_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                = "key-vault-${random_string.random.result}"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  sku_name            = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+  secret_permissions = [
+    "get",
+    "list"
+  ]
+  certificate_permissions = [
+    "get",
+    "list"
+  ]
+}
+
+resource "azurerm_ad_b2c_directory" "b2c_directory" {
+  name     = var.ad_b2c_directory_name
+  location = var.location
+}
+
+output "frontend_web_app_url" {
+  value = azurerm_app_service.app_service.default_site_hostname
+}
+
+output "backend_service_url" {
+  value = azurerm_app_service.backend_service.default_site_hostname
 }
