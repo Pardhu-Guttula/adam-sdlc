a1963333cd23b7c5349a1896a9bbdfdb78912669
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 18:08:18 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,22 +1,32 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
+  default     = "example-resources"
 }
+
 variable "location" {
-  description = "The Azure Region"
+  description = "The location of the resource group"
   type        = string
   default     = "West Europe"
 }
-variable "admin_login" {
-  description = "The SQL admin login name"
+
+variable "app_service_plan_id" {
+  description = "The App Service plan ID"
   type        = string
 }
-variable "admin_password" {
-  description = "The SQL admin password"
+
+variable "sql_admin_login" {
+  description = "The SQL server administrator login"
   type        = string
 }
-variable "log_analytics_workspace_sku" {
-  description = "The SKU of the Log Analytics workspace"
+
+variable "sql_admin_password" {
+  description = "The SQL server administrator password"
   type        = string
-  default     = "PerGB2018"
+  sensitive   = true
+}
+
+variable "role_principal_id" {
+  description = "The principal ID for the Azure role assignment"
+  type        = string
 }
