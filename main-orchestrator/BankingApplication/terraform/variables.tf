b3f7c6dad4098e11f630045b82b5a8b73750c1fc
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 15:40:12 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,9 +1,22 @@
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create resources."
+  description = "The name of the resource group"
   type        = string
 }
-
 variable "location" {
-  description = "The Azure location where resources should be created."
+  description = "The Azure Region"
   type        = string
+  default     = "West Europe"
+}
+variable "admin_login" {
+  description = "The SQL admin login name"
+  type        = string
+}
+variable "admin_password" {
+  description = "The SQL admin password"
+  type        = string
+}
+variable "log_analytics_workspace_sku" {
+  description = "The SKU of the Log Analytics workspace"
+  type        = string
+  default     = "PerGB2018"
 }
