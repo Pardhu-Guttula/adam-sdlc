c287119bcb28ed791068eba639ac1b5682b70557
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-10 13:22:11 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,16 +1,9 @@
-variable "location" {
-  description = "The location where resources will be created."
-  type        = string
-  default     = "East US"
-}
-
-variable "sql_admin_username" {
-  description = "The admin username for the SQL Server."
+variable "sql_admin_login" {
+  description = "The administrator login for the SQL server."
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The admin password for the SQL Server."
+  description = "The password for the SQL server administration."
   type        = string
-  sensitive   = true
 }
