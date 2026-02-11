76f6edcf0dca71c98f8e7bcc5c0f324c36caeb81
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-11 11:33:18 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/main.tf
+++ main-orchestrator/BankingApplication/terraform/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "East US"
+  location = "West Europe"
 }
 
 resource "azurerm_app_service_plan" "example" {
@@ -24,27 +24,74 @@ resource "azurerm_app_service" "example" {
   app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_ad_b2c_directory" "example" {
-  name                = "example-b2c-directory"
+resource "azurerm_frontdoor" "example" {
+  name                = "example-frontdoor"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_function_app" "example" {
+  name                = "example-function"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  storage_account_name = azurerm_storage_account.example.name
 }
 
 resource "azurerm_api_management" "example" {
-  name                = "example-api"
+  name                = "example-apim"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  publisher_name      = "example-publisher"
-  publisher_email     = "example@example.com"
-  sku_name            = "Developer_1"
+  sku_name            = "Consumption"
 }
 
-resource "azurerm_api_management_api" "example" {
-  name                = "example-api"
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "H@Sh1CoR3!"
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldb"
   resource_group_name = azurerm_resource_group.example.name
-  api_management_name = azurerm_api_management.example.name
-  revision            = "1"
-  display_name        = "Example API"
-  path                = "example"
-  protocols           = ["https"]
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+}
+
+resource "azurerm_logic_app" "example" {
+  name                = "example-logicapp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_eventgrid_topic" "example" {
+  name                = "example-eventgrid"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "example-keyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_subscription.example.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_security_center_contact" "example" {
+  email               = "security@example.com"
+  phone               = "1234567890"
+  alert_notifications = "On"
+  alerts_to_admins    = true
 }
