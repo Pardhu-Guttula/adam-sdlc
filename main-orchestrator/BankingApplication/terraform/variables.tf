cf55a2e8abc3034bb5ccde6dddc7fdeb677d3e69
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-11 11:33:19 +0530

    arch-gen

--- main-orchestrator/BankingApplication/terraform/variables.tf
+++ main-orchestrator/BankingApplication/terraform/variables.tf
@@ -1,11 +1,9 @@
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "example-resources"
+variable "location" {
+  description = "The location where resources will be created."
+  default     = "West Europe"
 }
 
-variable "location" {
-  description = "Location for all resources"
-  type        = string
-  default     = "East US"
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  default     = "example-resources"
 }
